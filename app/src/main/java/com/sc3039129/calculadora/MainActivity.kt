e94f2d548fa278c69294b6cef0e7b76daa059f96
Bia-Lara <agiampedro@gmail.com>
2025-08-27 21:58:34 -0300

    feat: adiciona entrada de numeros e botao de reset

--- app/src/main/java/com/sc3039129/calculadora/MainActivity.kt
+++ app/src/main/java/com/sc3039129/calculadora/MainActivity.kt
@@ -1,20 +1,69 @@
 package com.sc3039129.calculadora
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.sc3039129.calculadora.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val calculo = binding.calculo
+
+        binding.um.setOnClickListener{
+            calculo.text = "${calculo.text}1"
+        }
+
+        binding.dois.setOnClickListener{
+            calculo.text = "${calculo.text}2"
+        }
+
+        binding.tres.setOnClickListener{
+            calculo.text = "${calculo.text}3"
+        }
+
+        binding.quatro.setOnClickListener{
+            calculo.text = "${calculo.text}4"
+        }
+
+        binding.cinco.setOnClickListener{
+            calculo.text = "${calculo.text}5"
+        }
+
+        binding.seis.setOnClickListener{
+            calculo.text = "${calculo.text}6"
+        }
+
+        binding.sete.setOnClickListener{
+            calculo.text = "${calculo.text}7"
+        }
+
+        binding.oito.setOnClickListener{
+            calculo.text = "${calculo.text}8"
+        }
+
+        binding.nove.setOnClickListener{
+            calculo.text = "${calculo.text}9"
+        }
+
+        binding.zero.setOnClickListener{
+            calculo.text = "${calculo.text}0"
+        }
+
+        binding.ce.setOnClickListener {
+            calculo.text = ""
         }
     }
 }
